1e47ad6773524eb103f3783a106d3b319443febb
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 21:38:16 +0900

    プロジェクト ファイルを追加します。

---
+++ d5_underline/Program.cs
@@ -0,0 +1,11 @@
+﻿//https://paiza.jp/works/mondai/drankfast/d5_underline
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
